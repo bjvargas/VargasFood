1ba449ce5d74922a5acd6a5a2df496b2829e398c
Bruno Jaques Olivera de Vargas <70863493+bjvargas@users.noreply.github.com>
2022-05-20 20:13:29 -0300

    Using Lombok
    Using Lombok is controversial, but I chose to use this time, to have more functionality.

--- src/main/java/bj/vargas/vargasfood/domain/model/Kitchen.java
+++ src/main/java/bj/vargas/vargasfood/domain/model/Kitchen.java
@@ -1,14 +1,20 @@
 package bj.vargas.vargasfood.domain.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Kitchen {
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -16,37 +22,4 @@ public class Kitchen {
     @Column(length = 30)
     private String name;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        final Kitchen kitchen = (Kitchen) o;
-
-        if (id != null ? !id.equals(kitchen.id) : kitchen.id != null) return false;
-        return name != null ? name.equals(kitchen.name) : kitchen.name == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
-    }
 }
--- src/main/java/bj/vargas/vargasfood/domain/model/Restaurant.java
+++ src/main/java/bj/vargas/vargasfood/domain/model/Restaurant.java
@@ -1,5 +1,8 @@
 package bj.vargas.vargasfood.domain.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,9 +10,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Restaurant {
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,47 +25,4 @@ public class Restaurant {
 
     private BigDecimal shippingFee;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getShippingFee() {
-        return shippingFee;
-    }
-
-    public void setShippingFee(final BigDecimal shippingFee) {
-        this.shippingFee = shippingFee;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        final Restaurant that = (Restaurant) o;
-
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        if (name != null ? !name.equals(that.name) : that.name != null) return false;
-        return shippingFee != null ? shippingFee.equals(that.shippingFee) : that.shippingFee == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (shippingFee != null ? shippingFee.hashCode() : 0);
-        return result;
-    }
 }
